f43a577b24388f41a99e48e621130bc4e9b3b4a1
e9amohan <153479365+e9amohan@users.noreply.github.com>
2023-12-18 14:18:39 +0530

    completed

--- answer.py
+++ answer.py
@@ -1,5 +1,6 @@
 # What is the largest prime factor of the number 600,851,475,143?
 def answer(n):
+    """function for largest prime factor of the number 600,851,475,143"
     i = 2
     while i * i <= n:
         if n % i:
